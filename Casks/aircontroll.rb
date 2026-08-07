02616f99d3fd30ee88d83ab45b75f27b54ea1dff
Argyrios <310325170+argyrios-dev@users.noreply.github.com>
2026-08-07 12:28:14 +0200

    Update aircontroll.rb

--- Casks/aircontroll.rb
+++ Casks/aircontroll.rb
@@ -1,1 +1,10 @@
-
+cask "aircontroll" do
+  version "0.1.5"
+  sha256 "40a440f853ee99b7127838fd29c22514db07e489bdbfa79db29ac8cfddbda1a8"
+  url "https://github.com/argyrios-dev/AirControll/releases/download/v0.1.5/AirControll_0.1.5.dmg"
+  name "AirControll"
+  desc "Native macOS hand gesture control app using Vision and AVFoundation with fully offline recognition."
+  homepage "https://argyrios-dev.github.io/AirControll/"
+  depends_on macos: :ventura
+  app "AirControll.app"
+end
